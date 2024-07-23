4e3b607a88542bee3c0b50163b03d8aa2b16d731
Lokendra Tiwari <lokendratiwari22844@gmail.com>
2024-07-23 15:24:38 +0530

    Logout-Functionality:Done

--- client/src/routes/profile/Profile.jsx
+++ client/src/routes/profile/Profile.jsx
@@ -1,7 +1,20 @@
 import "./profile.scss";
 import List from "../../components/list/List";
 import Chat from "../../components/chat/Chat";
+import apiRequest from "../../../../api/lib/apiRequest";
+import { useNavigate } from "react-router-dom";
 function Profile() {
+  // const []
+  const navigate = useNavigate();
+  const handleLogout = async ()=>{
+    try{
+      const res = await apiRequest.post("/auth/logout");
+      localStorage.removeItem("user");
+      navigate("/");
+    }catch(error){
+      console.log(error);
+    }
+  }
   return (
     <div className="profile">
       <div className="details">
@@ -14,6 +27,7 @@ function Profile() {
            <span>Avatar: <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" /></span>
            <span>Usesname: <b>John Doe</b></span>
            <span>Email: <b>jondo@gmail.com</b></span>
+           <button onClick={handleLogout}>Logout</button>
           </div>
           <div className="title">
             <h1>My List</h1>
