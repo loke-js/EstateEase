fc9e042b8cac6682dd43f53879251d360a8cc63e
Lokendra Tiwari <lokendratiwari22844@gmail.com>
2024-07-23 15:11:22 +0530

    Integrated Register and Login Page with Api

--- api/app.js
+++ api/app.js
@@ -2,8 +2,12 @@ import express from 'express';
 import postRoute from './routes/post.route.js';
 import authRoute from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
-const app = express();
+import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
+const app = express();
+app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/post",postRoute);
--- client/src/routes/register/Register.jsx
+++ client/src/routes/register/Register.jsx
@@ -1,24 +1,51 @@
 import "./register.scss";
-import {Link} from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import apiRequest from "../../../../api/lib/apiRequest";
 function Register() {
+  const navigate = useNavigate();
+  
+  const [isLoading,setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    try {
+      const res = await apiRequest.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+      navigate("/login");
+    } catch (error) {
+      setError(error.response.data.message);
+    }finally{
+      setIsLoading(false);
+    }
+  };
   return (
     <div className="registerPage">
-    <div className="formContainer">
-      <form >
-        <h1>Create an Account</h1>
-        <input name="username" type="text" placeholder="Username" />
-        <input name="email" type="text" placeholder="Email" />
-        <input name="password" type="password" placeholder="Password" />
-        <button >Register</button>
-        <Link to="/login">Do you have an account?</Link>
-      </form>
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <h1>Create an Account</h1>
+          <input name="username" type="text" placeholder="Username" />
+          <input name="email" type="text" placeholder="Email" />
+          <input name="password" type="password" placeholder="Password" />
+          <button >Register</button>
+          {error && <span>{error}</span>}
+          <Link to="/login">Do you have an account?</Link>
+        </form>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
     </div>
-    <div className="imgContainer">
-      <img src="/bg.png" alt="" />
-    </div>
-  </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
