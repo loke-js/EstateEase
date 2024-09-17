83ecacd117a14a1821fc70e64fd3cfd4a5a8f4ab
Lokendra Tiwari <124779634+loke-js@users.noreply.github.com>
2024-09-18 00:10:48 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -11,7 +11,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
+app.use(cors({
+  origin: ["http://localhost:5173", "https://estateease-1.onrender.com"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/posts",postRoute);
@@ -20,6 +23,9 @@ app.use("/api/test",testRoute);
 app.use("/api/users",userRoute);
 app.use("/api/chats",chatRoute);
 app.use("/api/messages",messageRoute);
+app.get('/', (req, res) => {
+  res.send('EstateEase Backend is running!');
+});
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
